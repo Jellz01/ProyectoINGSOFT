4419902e490459bd9f70e68064e966c22ddf17b3
Joseph <Jellzwellesley@gmail.com>
2025-12-26 14:00:00 -0500

    refactor: Update deprecated declarative_base to new SQLAlchemy 2.0 syntax

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
+# Database setup and session management
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./mitienda.db"
 
